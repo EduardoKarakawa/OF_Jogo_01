e759aaa0a81c77f187f3f05a63ff45e21c17dd7e
EduardoKarakawa <eduardo.karakawa@hotmail.com>
2016-08-21 21:57:01 -0300

    "Jogo Completo"

--- OF_Jogo_01/src/ofApp.cpp
+++ OF_Jogo_01/src/ofApp.cpp
@@ -1,36 +1,214 @@
 #include "ofApp.h"
+#include <iostream>
+#include <time.h>
 
-string game_state;
 
-struct Player {
-	float posX, posY;
-	float velX, velY;
-	float radius = 15.0f;
-	float speed = 0.01f;
-	float atrito = 0.003f;
+int ShootFree(Shoot *shoot_temp) {
+	int i = 0;
+	while ((i < 10) && shoot_temp[i].vivo) {
+		i++;
+	}
+	return i;
+}
 
-	bool key_up, key_down, key_left, key_right;
-};
+void StartShoot(Shoot *shoot_temp, Player *p1_temp, int pos) {
+	shoot_temp[pos].posX = p1_temp->posX - 2.5f;
+	shoot_temp[pos].posY = p1_temp->posY - 35;
+	shoot_temp[pos].speed = SPEED_SHOOT;
+	shoot_temp[pos].vivo = true;
+}
 
-Player p1;
+void MovShoot(Shoot *shoot_temp) {
+	for (int i = 0; i < 10; i++) {
+		if (shoot_temp[i].vivo) {
+			if (shoot_temp[i].velY < VEL_SHOOT) {
+				shoot_temp[i].velY += shoot_temp[i].speed;
+			}
+
+			shoot_temp[i].posY -= shoot_temp[i].velY;
+
+			if (shoot_temp[i].posY < 0) {
+				shoot_temp[i].velY = 0;
+				shoot_temp[i].vivo = false;
+			}
+		}
+	}
+}
+
+int SeeTotalEnemyLive(Player *enemy_temp) {
+	int cont = 0;
+	int i = 0, f = 4;
+	while (i <= f){
+		if ((enemy_temp + i)->vivo || (enemy_temp + i)->vivo) {
+			cont++;
+		}
+		i++;
+		f--;
+	}
+	return cont;
+}
+
+void SpawEnemy(Player *enemy_temp) {
+	for (int i = 0; i < 5; i++) {
+		(enemy_temp + i)->posX = 30 + rand() % 934;
+		(enemy_temp + i)->posY = - rand() % 90;
+		(enemy_temp + i)->prox_posX = 30 + rand() % 934;
+		(enemy_temp + i)->speed = 0.01f;
+		(enemy_temp + i)->vivo = true;
+		(enemy_temp + i)->r = rand() % 255;
+		(enemy_temp + i)->g = rand() % 255;
+		(enemy_temp + i)->b = rand() % 255;
+	}
+}
+
+void MovEnemy(Player *enemy_t) {
+	if (enemy_t->velX < 3) {
+		enemy_t->velX += enemy_t->speed;
+	}
+
+	if (enemy_t->velY < 0.7) {
+		enemy_t->velY += enemy_t->speed;
+	}
+
+	if (enemy_t->posX > enemy_t->prox_posX) {
+		enemy_t->posX -= enemy_t->velX;
+		if (enemy_t->posX <= enemy_t->prox_posX)
+			enemy_t->prox_posX = 30 + rand() % 934;
+	}
+
+	if (enemy_t->posX < enemy_t->prox_posX) {
+		enemy_t->posX += enemy_t->velX;
+		if (enemy_t->posX >= enemy_t->prox_posX)
+			enemy_t->prox_posX = 30 + rand() % 934;
+	}
+
+	if (enemy_t->posY > 780) {
+		enemy_t->vivo = false;
+	}
+	
+	enemy_t->posY += enemy_t->velY;
+}
+
+void ControlEnemy(Player *enemy_temp) {
+	if (SeeTotalEnemyLive(enemy_temp) == 0) {
+		SpawEnemy(enemy_temp);
+	}
+
+	else {
+		for (int i = 0; i < 5; i++) {
+			if ((enemy_temp + i)->vivo) {
+				MovEnemy(&enemy_temp[i]);
+			}
+		}
+	}
+}
+
+void Atrito(Player *p) {
+	if (p->velX > 0) {
+		p->velX -= p->atrito;
+		if (p->velX < 0)
+			p->velX = 0;
+	}
+	if (p->velX < 0) {
+		p->velX += p->atrito;
+		if (p->velX > 0)
+			p->velX = 0;
+	}
+	if (p->velY > 0) {
+		p->velY -= p->atrito;
+		if (p->velY < 0)
+			p->velY = 0;
+	}
+	if (p->velY < 0) {
+		p->velY += p->atrito;
+		if (p->velY > 0)
+			p->velY = 0;
+	}
+}
+
+void VerSeApertou(InputPlayer *key_temp, Player *p_temp) {
+	if (key_temp->right && p_temp->velX < VEL)
+		p_temp->velX = p_temp->velX + p_temp->speed;
+
+	if (key_temp->left && p_temp->velX > -VEL)
+		p_temp->velX = p_temp->velX - p_temp->speed;
+
+	if (key_temp->up && p_temp->velY > -VEL)
+		p_temp->velY = p_temp->velY - p_temp->speed;
+
+	if (key_temp->down && p_temp->velY < VEL)
+		p_temp->velY = p_temp->velY + p_temp->speed;
+}
+
+void ControlPlayer(Player *p_temp, InputPlayer *key_temp) {
+	//atualiza movimentação
+	p_temp->posX += p_temp->velX;
+	p_temp->posY += p_temp->velY;
+
+
+	//regras de atrito
+	if (!key_temp->down && !key_temp->left && !key_temp->right && !key_temp->up) {
+		Atrito(p_temp);
+	}
+
+	//verifica se ta apertado
+	VerSeApertou(key_temp, p_temp);
+
+
+}
+
+void ColisionEnemyToShoot(Player *enemy_temp, Shoot *shoot_temp) {
+	for (int i = 0; i < 10; i++) {
+		if ((shoot_temp + i)->vivo) {
+			for (int j = 0; j < 5; j++) {
+				if (((shoot_temp + i)->posX >= (enemy_temp + j)->posX - 20) && ((shoot_temp + i)->posX <= (enemy_temp + j)->posX + 20)) {
+					if (((shoot_temp + i)->posY - 10 >= (enemy_temp + j)->posY - 12) && ((shoot_temp + i)->posY - 10 <= (enemy_temp + j)->posY + 12)) {
+						(enemy_temp + j)->vivo = false;
+						(shoot_temp + i)->vivo = false;
+					}
+				}
+			}
+		}
+	}
+}
 
 //--------------------------------------------------------------
 void ofApp::setup() {
-	//estado do jogo
+	srand(time(NULL));
+
+//estado do jogo
 	game_state = "game";
 
-	//inicia a posição do player
+//inicia a posição do player
 	p1.posX = 400;
 	p1.posY = 400;
+	p1.vivo = true;
 
-
-	//velocidade do player
 }
 
 //--------------------------------------------------------------
 void ofApp::update() { // executa a 60fps
 
-					   //reflete ao atintegir limite de tela
+//contagem de fps
+	if (fps < 120)
+		fps++;
+	else {
+		fps = 0;
+	}
+
+	
+
+//Controlar o player
+	ControlPlayer(&p1, &inp_key);
+
+
+
+//Controla inimigo
+	ControlEnemy(enemy);
+
+
+	
+//reflete ao atintegir limite de tela
 	if ((p1.posX <= (p1.radius)) || (p1.posX >= (1024 - p1.radius)))
 		p1.velX = -p1.velX;
 
@@ -38,58 +216,62 @@ void ofApp::update() { // executa a 60fps
 		p1.velY = -p1.velY;
 
 
-	//atualiza movimentação
-	p1.posX += p1.velX;
-	p1.posY += p1.velY;
+		
+//time para tiro
+	if(p1.time <= 20)
+		p1.time++;
 
 
-	//verifica se ta apertado
-	if (p1.key_right && p1.velX < 1)
-		p1.velX = p1.velX + p1.speed;
 
-	if (p1.key_left && p1.velX > -1)
-		p1.velX = p1.velX - p1.speed;
-
-	if (p1.key_up && p1.velY > -1)
-		p1.velY = p1.velY - p1.speed;
-
-	if (p1.key_down && p1.velY < 1)
-		p1.velY = p1.velY + p1.speed;
-
-
-	//regras de atrito
-	if (!p1.key_down && !p1.key_left && !p1.key_right && !p1.key_up) {
-		if (p1.velX > 0) {
-			p1.velX -= p1.atrito;
-			if (p1.velX < 0)
-				p1.velX = 0;
-		}
-		if (p1.velX < 0) {
-			p1.velX += p1.atrito;
-			if (p1.velX > 0)
-				p1.velX = 0;
-		}
-		if (p1.velY > 0) {
-			p1.velY -= p1.atrito;
-			if (p1.velY < 0)
-				p1.velY = 0;
-		}
-		if (p1.velY < 0) {
-			p1.velY += p1.atrito;
-			if (p1.velY > 0)
-				p1.velY = 0;
-		}
+//Verifica se tem algum tiro na tela e movimenta ele
+	if (inp_key.space && (p1.time >= 20)){
+		p1.time = 0;
+		StartShoot(shoot, &p1, ShootFree(shoot));
 	}
+	MovShoot(shoot);
+
+//Colisao de tiro com inimigo
+	ColisionEnemyToShoot(enemy, shoot);
 
 
 	
+	
+		
 
 }
 
 
 //--------------------------------------------------------------
 void ofApp::draw() {
+//Cor de fundo
+	ofBackground(6, 8, 26);
+	
+
+//Imprime os tiros
+	for (int i = 0; i < 10; i++) {
+		if (shoot[i].vivo) {
+			ofSetColor(225, 168, 00);
+			ofDrawRectangle(shoot[i].posX, shoot[i].posY, 5, 20);
+		}
+	}
+
+//Imprime os inimigos
+	for (int i = 0; i < 5; i++) {
+		if (enemy[i].vivo) {
+			ofSetColor(enemy[i].r, enemy[i].g, enemy[i].b);
+			ofDrawRectangle(enemy[i].posX, enemy[i].posY, 40, 24);
+		}
+	}
+
+
+//Imprime o personagem
+	ofSetColor(140, 135, 130);
+	ofDrawTriangle(p1.posX-15, p1.posY, p1.posX+15, p1.posY, p1.posX, p1.posY - 30);
+
+	ofSetColor(10, 127, 250);
 	ofDrawCircle(p1.posX, p1.posY, p1.radius);
+	
+	
 }
 
 
@@ -98,13 +280,15 @@ void ofApp::keyPressed(int key) {
 	if (game_state == "game") {
 
 		if (key == OF_KEY_LEFT)
-			p1.key_left = true;
+			inp_key.left = true;
 		if (key == OF_KEY_RIGHT)
-			p1.key_right = true;
+			inp_key.right = true;
 		if (key == OF_KEY_UP)
-			p1.key_up = true;
+			inp_key.up = true;
 		if (key == OF_KEY_DOWN)
-			p1.key_down = true;
+			inp_key.down = true;
+		if (key == ' ')
+			inp_key.space = true;
 	}
 }
 
@@ -113,13 +297,15 @@ void ofApp::keyPressed(int key) {
 void ofApp::keyReleased(int key) {
 	if (game_state == "game") {
 		if (key == OF_KEY_LEFT)
-			p1.key_left = false;
+			inp_key.left = false;
 		if (key == OF_KEY_RIGHT)
-			p1.key_right = false;
+			inp_key.right = false;
 		if (key == OF_KEY_UP)
-			p1.key_up = false;
+			inp_key.up = false;
 		if (key == OF_KEY_DOWN)
-			p1.key_down = false;
+			inp_key.down = false;
+		if (key == ' ')
+			inp_key.space = false;
 	}
 }
 
--- OF_Jogo_01/src/ofApp.h
+++ OF_Jogo_01/src/ofApp.h
@@ -2,7 +2,43 @@
 
 #include "ofMain.h"
 
+#define VEL 9
+#define VEL_SHOOT 50
+#define SPEED_SHOOT 1
+
+
+struct Player {
+	float posX, posY, prox_posX;
+	float velX = 0, velY = 0;
+	float radius = 15.0f;
+	float speed = 0.5f;
+	float atrito = 0.1f;
+	int r, g, b;
+	int time = 0;
+	bool vivo;
+};
+
+struct Shoot {
+	float posX, posY;
+	float velY = 0;
+	float speed = SPEED_SHOOT;
+	bool vivo;
+};
+
+struct InputPlayer {
+	bool up, down, left, right;
+	bool space;
+};
+
+
 class ofApp : public ofBaseApp{
+
+	string game_state;
+	Player p1;
+	Player enemy[5];
+	Shoot shoot[10];
+	InputPlayer inp_key;
+	int fps = 0;
 
 	public:
 		void setup();
